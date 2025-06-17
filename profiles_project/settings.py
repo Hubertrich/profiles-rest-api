77c02eadf31ff6f01563e4ab7b6543c187e47375
Hubertrich <hubertrichardo1703@gmail.com>
2025-06-18 02:08:09 +0700

    set up media folder and URL routing for downloadable Excel output

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -128,3 +128,8 @@ STATIC_ROOT = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -22,3 +24,6 @@ urlpatterns = [
     path('api/', include('profiles_api.urls')),
     path('api/excel/', include('excel_upload_features.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
